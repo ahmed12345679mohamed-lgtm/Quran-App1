a45e04d59711854251d2aae155446f0a6850f237
ahmed12345679mohamed-lgtm <ahmed12345679mohamed@gmail.com>
2025-12-01 10:37:46 +0200

    Add files via upload

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// إعدادات مشروعك الخاصة (تم دمج المفاتيح التي أرسلتها)
 const firebaseConfig = {
   apiKey: "AIzaSyBy9-kDy0JnunaSubLm-VhliTGhP2jZs6o",
   authDomain: "dar-altawheed.firebaseapp.com",
@@ -8,3 +11,9 @@ const firebaseConfig = {
   appId: "1:1090036818546:web:2439dbc444658f5c4698eb",
   measurementId: "G-3DVF71VRBN"
 };
+
+// تهيئة Firebase
+const app = initializeApp(firebaseConfig);
+
+// تصدير قاعدة البيانات لاستخدامها في باقي التطبيق
+export const db = getFirestore(app);
