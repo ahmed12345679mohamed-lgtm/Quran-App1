a49c199f4621b43b7dcd0a5a6500ae51685eef21
ahmed12345679mohamed-lgtm <ahmed12345679mohamed@gmail.com>
2025-12-10 16:29:16 +0200

    Add files via upload

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,6 +1,10 @@
 
-// FIREBASE DISABLED FOR UI TESTING
-// This file is currently empty to prevent connection errors in the preview environment.
-// To reconnect: Uncomment the firebase initialization code when ready for backend development.
-
-export const db = {} as any;
+const firebaseConfig = {
+  apiKey: "AIzaSyBy9-kDy0JnunaSubLm-VhliTGhP2jZs6o",
+  authDomain: "dar-altawheed.firebaseapp.com",
+  projectId: "dar-altawheed",
+  storageBucket: "dar-altawheed.firebasestorage.app",
+  messagingSenderId: "1090036818546",
+  appId: "1:1090036818546:web:2439dbc444658f5c4698eb",
+  measurementId: "G-3DVF71VRBN"
+};
